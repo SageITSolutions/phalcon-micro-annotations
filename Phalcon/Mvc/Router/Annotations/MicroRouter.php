cb8ab22b5ef48ce4c0fc31e8ed4074187d60d8f0
Daniel S. Davis <daniel.davis@sageitsolutions.net>
2022-12-28 11:48:20 -0500

    Initial Release completed

--- Phalcon/Mvc/Router/Annotations/MicroRouter.php
+++ Phalcon/Mvc/Router/Annotations/MicroRouter.php
@@ -2,38 +2,52 @@
 namespace Phalcon\Mvc\Router\Annotations;
 
 /**
- * Phalcon\Db\Adapter\Pdo\Sqlsrv
- * Specific functions for the MsSQL database system
+ * Phalcon\Mvc\Router\Annotations
+ * Replacement Router for Micro app that supports route annotations
  * <code>
- * $config = array(
- * "host" => "192.168.0.11",
- * "dbname" => "blog",
- * "port" => 3306,
- * "username" => "sigma",
- * "password" => "secret"
+ * $option = (object)[
+ *   "adapter" => 'stream',                 // 'apcu','stream','memory'
+ *   "directory" => "app/controllers/",
+ *   "namespace" => "app\controllers\\",
+ *   "lifetime" => 3600,                    // Default is 6 hours
+ *   "cachedirectory" => "/app/storage/cache/annotations"
  * );
- * $connection = new \Phalcon\Db\Adapter\Pdo\Sqlsrv($config);
+ * $router = new \Phalcon\Mvc\Router\Annotations\MicroRouter($di,$option);
  * </code>.
  *
- * @property \Phalcon\Db\Dialect\Sqlsrv $_dialect
+ * @property stdClass $options
+ * @property Mixed $adapter
+ * @property array $collections
+ * @property \Phalcon\Logger\Logger $logger
  */
-class MicroRouter extends \Phalcon\Di\Injectable
+class MicroRouter extends \Phalcon\Mvc\Router
 {
     protected $options;
     protected $adapter;
+    protected $collections = array();
+    protected $logger;
 
-    public function __construct(\stdClass $options)
+    /**
+     * Creates new MicroRouter
+     * @param \Phalcon\Di\Di $di
+     * @param \stdClass $options
+     */
+    public function __construct(\Phalcon\Di\Di $di, \stdClass $options)
     {
+        parent::__construct(true);
+        $this->removeExtraSlashes(true);
+        $this->setDI($di);
+        $this->logger = $this->getDI()->get('logger');
         $this->options = $this->defaultOptions($options);
         switch ($this->options->adapter) {
             case "apcu":
-                $adapter = new \Phalcon\Annotations\Adapter\Apcu([
-                    'prefix' => 'micro_routes',
+                $this->adapter = new \Phalcon\Annotations\Adapter\Apcu([
+                    'prefix' => '_micro-router_',
                     'lifetime' => $this->options->lifetime,
                 ]);
                 break;
             case "stream":
-                $adapter = new \Phalcon\Annotations\Adapter\Stream([
+                $this->adapter = new \Phalcon\Annotations\Adapter\Stream([
                     'annotationsDir' => $this->options->cachedirectory
                 ]);
                 break;
@@ -48,46 +62,164 @@ class MicroRouter extends \Phalcon\Di\Injectable
      * @param \stdClass $options
      * @return \stdClass
      */
-    private function defaultOptions(\stdClass $options): \stdClass
+    protected function defaultOptions(\stdClass $options): \stdClass
     {
         $opt = (object) [
-            "adapter" => (apcu_enabled() ? 'apcu' : 'stream'),
+            "adapter" => "apcu",
             "directory" => "app/controllers/",
-            "namespace" => "app\controllers\\",
+            "namespace" => "App\Controllers",
             "lifetime" => 21600000,
-            "cachedirectory" => "/app/storage/cache/annotations"
+            "cachedirectory" => "/app/storage/cache/annotations/"
         ];
 
         $obj = (object) array_merge((array) $opt, (array) $options);
-        if ($obj->adapter == 'apcu' && !apcu_enabled())
+        if ($obj->adapter == 'apcu' && !(function_exists('apcu_enabled') && apcu_enabled()))
             $obj->adapter = "stream";
         if ($obj->adapter == 'stream' && !is_dir($obj->cachedirectory))
             $obj->adapter = "memory";
 
+        $this->setDefaults(
+            array(
+                'namespace' => $obj->namespace,
+                'controller' => 'index',
+                'action' => 'index'
+            )
+        );
+
         return $obj;
     }
 
+    /**
+     * Primary Method to populate routes
+     * @return void
+     */
     protected function setRoutes(): void
     {
-        $adapter = new \Phalcon\Annotations\Adapter\Memory();
-        foreach (preg_grep('~\.(php)$~', scandir(BASE_PATH . 'app/controllers/')) as $controller_file) {
-            $className = str_replace('.php', '', $controller_file);
-            $this->processReflector($className, $adapter->get('App\Controllers\\' . $className));
+        $classes = [];
+        switch ($this->options->adapter) {
+            case "apcu":
+                $iterator = new \APCuIterator("/^_phan/");
+
+                if (true === is_object($iterator)) {
+                    $this->logger->debug('Iterating APCu');
+                    foreach ($iterator as $item) {
+                        $className = end(explode('\\', $item["key"]));
+                        $reflector = str_replace('_phan_micro-router_', '', $item["key"]);
+                        $classes[$className] = $reflector;
+                    }
+                }
+                break;
+            case "stream":
+                $this->logger->debug('Iterating Cache Files');
+                foreach (preg_grep('~\.(php)$~', scandir($this->options->cachedirectory)) as $cache_file) {
+                    $className = str_replace('.php', '', end(explode('_', $cache_file)));
+                    $reflector = str_replace('_', '\\', str_replace('.php', '', $cache_file));
+                    $classes[$className] = $reflector;
+                }
+                break;
         }
-        foreach ($this->collections as $col) {
-            $this->mount($col);
+
+        if (empty($classes)) {
+            $this->logger->debug('Classes Empty, Parsing Files');
+            foreach (preg_grep('~\.(php)$~', scandir($this->options->directory)) as $controller_file) {
+                $className = str_replace('.php', '', $controller_file);
+                $reflector = $this->options->namespace . '\\' . $className;
+                $classes[$className] = $reflector;
+            }
+        }
+
+        foreach ($classes as $className => $reflector) {
+            $this->processReflector($className, $this->adapter->get($reflector));
         }
     }
 
-    private function processReflector(string $className, \Phalcon\Annotations\Reflection $reflection = null)
+    /**
+     * Clears Cache in the event of a need to rebuilt routes
+     * @throws \Exception 
+     * @return bool
+     */
+    public function clearCache(): bool
+    {
+        try {
+            switch ($this->options->adapter) {
+                case "apcu":
+                    $iterator = new \APCuIterator("/^_phan/");
+
+                    if (false === is_object($iterator)) {
+                        throw new \Exception("APCu Object not defined", 500);
+                    }
+
+                    foreach ($iterator as $item) {
+                        if (true !== apcu_delete($item["key"])) {
+                            throw new \Exception(sprintf("Unable to Delete '%s'", $item["key"]), 500);
+                        }
+                    }
+
+                    if ($this->logger) {
+                        $this->logger->notice(sprintf("APCu Cache Cleared for '/^_phan/'", ));
+                    }
+                    break;
+                case "stream":
+                    foreach (preg_grep('~\.(php)$~', scandir($this->options->cachedirectory)) as $cache_file) {
+                        $delfile = $this->options->cachedirectory . $cache_file;
+                        if (is_file($delfile)) {
+                            unlink($delfile);
+                        }
+                    }
+                    if ($this->logger) {
+                        $this->logger->notice(sprintf("Files Cleared From: '%s'", $this->options->cachedirectory));
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        } catch (\Exception $e) {
+            if ($this->logger) {
+                $this->logger->error(sprintf('Error encountered clearing Cache: %s', $e->getMessage()));
+            }
+        } catch (\Error $e) {
+            if ($this->logger) {
+                $this->logger->error(sprintf('Error encountered clearing Cache: %s', $e->getMessage()));
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Parses Routes to mount into provided Micro App
+     * @param \Phalcon\Mvc\Micro $micro
+     * @return void
+     */
+    public function mountMicro(\Phalcon\Mvc\Micro $micro): void
+    {
+        foreach ($this->collections as $key => $col) {
+            $micro->mount($col);
+        }
+    }
+
+    /**
+     * Processes Reflector Class to identify Annotations
+     * @param string $className
+     * @param \Phalcon\Annotations\Reflection|null $reflection
+     * @return void
+     */
+    protected function processReflector(string $className, \Phalcon\Annotations\Reflection $reflection = null)
     {
         if (!$reflection)
             return;
+
         $prefix = $this->processClassAnnotations($className, $reflection->getClassAnnotations());
         $this->processMethodAnnotations($className, $prefix, $reflection->getMethodsAnnotations());
     }
 
-    private function processClassAnnotations(string $className, \Phalcon\Annotations\Collection $collection = null): string
+    /**
+     * Process Annotations for Class header
+     * @param string $className
+     * @param \Phalcon\Annotations\Collection|null $collection
+     * @return string
+     */
+    protected function processClassAnnotations(string $className, \Phalcon\Annotations\Collection $collection = null): string
     {
         if (!$collection)
             return '';
@@ -125,11 +257,20 @@ class MicroRouter extends \Phalcon\Di\Injectable
         return $prefix;
     }
 
-    private function processMethodAnnotations(string $className, string $prefix, array $collection = null): void
+    /**
+     * Process Annotations for Class Methods
+     * @param string $className
+     * @param string $prefix
+     * @param array|null $collection
+     * @throws \Phalcon\Exception 
+     * @return void
+     */
+    protected function processMethodAnnotations(string $className, string $prefix, array $collection = null): void
     {
         if (!$collection)
             return;
 
+        //$this->logger->debug(sprintf("processMethodAnnotations for: '%s' with prefix: '%s'", $className, $prefix));
         foreach ($collection as $function => $annotations) {
             // loop the annotations for the current function
             foreach ($annotations as $ann) {
@@ -186,8 +327,9 @@ class MicroRouter extends \Phalcon\Di\Injectable
     private function addRoute($route, $request, $class, $method, $name = null)
     {
         if (!isset($this->collections[$class])) {
+            $handler = $this->options->namespace . '\\' . $class;
             $this->collections[$class] = new \Phalcon\Mvc\Micro\Collection();
-            $this->collections[$class]->setHandler('App\Controllers\\' . $class)->setLazy(true);
+            $this->collections[$class]->setHandler($handler)->setLazy(true);
         }
 
         // always remove trailing slash
@@ -197,9 +339,5 @@ class MicroRouter extends \Phalcon\Di\Injectable
 
         $verb = strtolower($request);
         $this->collections[$class]->$verb($route, $method, $name);
-    }
-    public function run()
-    {
-        $this->handle(str_replace($this->base_uri, '/', $_SERVER["REQUEST_URI"]));
     }
 }
